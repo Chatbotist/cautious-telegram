acc14d98c6d39c1b5fa2e5b060d368dc8b136bcd
Дмитрий <143331027+Chatbotist@users.noreply.github.com>
2024-06-20 05:22:19 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async списоксобытий1_Event4_Act3(runtime, localVars)
+	{
+		alert("Запрос успешно обработан")
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
